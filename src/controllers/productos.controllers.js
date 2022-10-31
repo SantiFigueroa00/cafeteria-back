34f92a7583962655a693df6a5cad7ed4cd45339b
Santiago <santiagofig00@gmail.com>
2022-10-31 03:23:50 -0300

    agregamos el codigo para agregar producto

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,10 +1,25 @@
+import Producto from "../models/producto,js";
+
 export const listarProductos = (req, res) => {
   res.send("hola desde el backend en la peticion get");
 };
 
-export const crearProducto = (req, res) => {
-    console.log(req.body)
+export const crearProducto = async (req, res) => {
+  try {
+    console.log(req.body);
     //validar objeto
+    const productoNuevo = new Producto(req.body);
     //guardar el objeto en la base de datos
+    await productoNuevo.save();
+    res.status(201).json({
+      mensaje: "El producto fue creado",
+    });
+
     res.send("aqui tendria que crear un producto");
+  } catch (error) {
+    console.log(error);
+    res.status(201).json({
+      mensaje: "error al intentar agregar un producto",
+    });
+  }
 };
