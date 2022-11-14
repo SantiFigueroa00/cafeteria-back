41658de9b345004749deb2e7f35e1b9910b42457
Santiago <santiagofig00@gmail.com>
2022-11-14 11:08:28 -0300

    Agrego las rutas en index.js

--- src/index.js
+++ src/index.js
@@ -1,38 +1,41 @@
-import express from "express";
-import cors from 'cors'
-import morgan from "morgan";
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import path from 'path';
 import productoRouter from './routes/productos.routes'
-import path from "path";
-//conxion con la DB
+import authRouter from './routes/usuarios.routes'
+import * as dotenv from 'dotenv';
+//llamar a la conexion a la bd
 import './database';
 
+dotenv.config();
 
 //crear una instancia de express
 const app = express();
-
 //configurar un puerto
 app.set('port', process.env.PORT || 4000);
 
-app.listen(app.get('port'),()=>{
-    console.log('estoy en el purto '+ app.get('port'))
+app.listen( app.get('port'),()=>{
+    console.log('Estoy en el puerto '+  app.get('port'));
 })
 
-//middlewares: func que se ejeccutan antes de llegar a la rutas
-app.use(cors());//permite peticiones remotas}
-
-//permite reciir y usar objetos en formato json
+//middlewares: son funciones que se ejecutan antes de llegar a las rutas
+app.use(cors()); //permite peticiones remotas
+//permite recibir y usar objetos en formato json
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-//indormacion extra
+app.use(express.urlencoded({extended:true}))
+//informacion extra
 app.use(morgan('dev'))
-// cargar un archivo estatico
+//cargar un archivo estatico
 app.use(express.static(path.join(__dirname,'../public')))
-console.log(path.join(__dirname,'../public'))
-
 
 //rutas
-// app.get('/prueba',(req,res)=>{
+// app.get('/prueba', (req, res)=>{
 //     res.send('hola desde el backend en la peticion get')
 // })
-
+//http://localhost:4000/apicafe/prueba
 app.use('/apicafe',productoRouter)
+// http://localhost:4000/apicafe/auth/
+app.use('/apicafe/auth',authRouter)
+
+
